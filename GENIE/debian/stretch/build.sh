2aa4cfe8b1b8381007315c2eb97e74927aa38075
Luke Pickering <luketpickering@googlemail.com>
2019-10-01 00:13:15 -0500

    adds version subdirectories to all installed software. ROOT is now installed to opt.

--- GENIE/debian/stretch/build.sh
+++ GENIE/debian/stretch/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker build --squash --tag picker24/genie_2_12_2_root_5:centos_7 .
+sudo docker build --squash --tag picker24/genie_2_12_2:debian_stretch .
--- T2KReWeight/debian/stretch/build.sh
+++ T2KReWeight/debian/stretch/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker build --squash --tag picker24/t2kreweight:debian_stretch .
+sudo docker build --squash --tag picker24/t2kreweight_v1r27p3:debian_stretch .
