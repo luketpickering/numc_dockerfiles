cae3d7a9910ba810c97395ea630227461b542925
Luke Pickering <luketpickering@googlemail.com>
2019-10-06 00:20:55 -0500

    Updates bashbuild.ah tree, up to NOvARwgt now works!

--- buildah/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ctr=$(buildah from scratch)
-mnt=$(buildah mount $ctr)
-echo "hi!"
-buildah config --entrypoint="/bin/PACKAGE" --env "FOO=BAR" $ctr
-buildah commit $ctr IMAGENAME
-buildah unmount $ctr
