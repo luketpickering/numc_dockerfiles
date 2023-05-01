d2752d8fa8e685e23293f256ca44808db213f592
Luke Pickering <luketpickering@gmail.com>
2023-05-01 22:23:45 +0100

    removes buildah scripts

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-Some buildah scripts for building neutrino MC tooling
+Some dockerfiles for building neutrino MC tooling
 
-Relies on bashbuildah: https://github.com/luketpickering/bashbuildah.git
-
-For the NEUT generator and the experiment-specific reweighting
-code, you will need to get the sources elsewhere.
+For experiment-specific code, you may need to be added to relevant repositories
---
+++ alma/highland_2_89/buildmultiarch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+set -x
+
+docker buildx build . -t picker24/highland_2_89:alma9-aarch64 \
+                       --platform=linux/arm64 --build-arg ND280ARCH=aarch64 \
+                       --load --ssh default
+# docker push picker24/highland_2_89:alma9-aarch64
+
+# docker buildx build . -t picker24/highland_2_89:alma9-x86_64 \
+#                        --platform=linux/amd64 --build-arg ND280ARCH=x86_64 \
+#                        --load --ssh default
+# docker push picker24/highland_2_89:alma9-x86_64
+
+# docker manifest rm picker24/highland_2_89:alma9 || true
+# docker manifest create picker24/highland_2_89:alma9 \
+#                 --amend picker24/highland_2_89:alma9-aarch64 \
+#                 --amend picker24/highland_2_89:alma9-x86_64
+# docker manifest push picker24/highland_2_89:alma9
