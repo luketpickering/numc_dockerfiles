a4300dca03b06aff922e8d36b6ef2e4cd50c0bf2
Luke Pickering <luketpickering@gmail.com>
2023-05-01 22:19:26 +0100

    may 23 updates.

--- dockerfiles/debian/build_t2kreweight.sh
+++ dockerfiles/debian/build_t2kreweight.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# eval $(ssh-agent)
+# ssh-add
+# buildx build . -t picker24/psyche_3_73:debian_bullseye-slim --ssh default --build-arg ND280ARCH=aarch64
+# --platform=linux/amd64,linux/arm64
+
 for i in buildbox root_v6_24_06; do # neut_oa2021 t2kreweight_oa2021; do 
 	cd ${i};
 	docker build . -t picker24/${i}:debian_bullseye-slim;
