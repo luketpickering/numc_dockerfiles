38a65415face96bc8ce9d55e9b4ae7edaeb4a1fc
Luke Pickering <luketpickering@gmail.com>
2024-07-05 14:37:41 +0100

    Adds more alma9 tools for pittpacc workshop

---
+++ alma9/nuisance2_git_master/buildmultiarch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+set -x
+
+docker buildx build . -t picker24/nuisance2_git_master:alma9-amd64 \
+                       --platform=linux/amd64 \
+                       --load
+docker push picker24/nuisance2_git_master:alma9-amd64
+
+docker buildx build . -t picker24/nuisance2_git_master:alma9-arm64 \
+                       --platform=linux/arm64 \
+                       --load
+docker push picker24/nuisance2_git_master:alma9-arm64
+
+docker manifest rm picker24/nuisance2_git_master:alma9 || true
+docker manifest create picker24/nuisance2_git_master:alma9 \
+                --amend picker24/nuisance2_git_master:alma9-arm64 \
+                --amend picker24/nuisance2_git_master:alma9-amd64
+docker manifest push picker24/nuisance2_git_master:alma9
